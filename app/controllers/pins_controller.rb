7430039849cff7bfb66f64983ce3d9e990aaf802
Prabal Dutta <wizard19@gmail.com>
2015-03-23 00:33:25 -0500

    json deleted

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,20 +1,18 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
   def index
     @pins = Pin.all
-    respond_with(@pins)
+    
   end
 
   def show
-    respond_with(@pin)
+
   end
 
   def new
     @pin = Pin.new
-    respond_with(@pin)
+  
   end
 
   def edit
@@ -23,17 +21,17 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     @pin.save
-    respond_with(@pin)
+    
   end
 
   def update
     @pin.update(pin_params)
-    respond_with(@pin)
+  
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+  
   end
 
   private
